11b882ba71325e8ebafdd191323180550263efb7
Jeremie Madara <jeremiemadara@gmail.com>
2021-03-22 14:46:31 +0100

    all the files

---
+++ src/components/robots.js
@@ -0,0 +1,27 @@
+export const robots = [
+    {
+        id:1,
+        name: 'Jeremie Valili 2',
+        username: 'JereVal',
+        email: 'Jeremievalili.211@vmail.com'
+    },
+    {
+        id:2,
+        name: 'Junior Mbollo',
+        username: 'M9a1',
+        email: 'Junior9a1@kmail.com'
+    },
+    {
+        id:3,
+        name: 'Charles Deen',
+        username: 'Demeter',
+        email: 'CharlesRapoutine@rmail.com'
+    },
+    {
+        id:4,
+        name: 'Tomy High',
+        username: 'Tommy',
+        email: 'Tomytine@rmail.com'
+    },
+]
+
